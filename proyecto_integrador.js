0f65088f3fb04e5534c9b3fbb610e318eb02a3b4
Rubia <rj-rosales@hotmail.com>
2022-06-16 21:01:19 -0300

    Proyecto integrador: Navbar v1

---
+++ proyecto_integrador.js
@@ -0,0 +1,21 @@
+const abrirMenuBtn = document.querySelector(".abrirMenu-responsive");
+const cerrarMenuBtn = document.querySelector(".cerrarMenu-responsive");
+const redesSociales = document.querySelector(".social-bar");
+
+function abrir() {
+
+    redesSociales.classList.toggle("menu-abierto");
+
+
+
+}
+function cerrar() {
+
+    redesSociales.classList.toggle("menu-abierto");
+
+
+
+}
+
+abrirMenuBtn.addEventListener("click",abrir);
+cerrarMenuBtn.addEventListener("click",cerrar);
