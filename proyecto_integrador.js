5dfe684af8fbf7a505a983b82e99bc7302f041ed
Rubia <rj-rosales@hotmail.com>
2022-06-20 17:48:26 -0300

    Banner simple y foto de perfil

--- proyecto_integrador.js
+++ proyecto_integrador.js
@@ -1,23 +1,16 @@
+// SCRIPT DEL HEADER
 const abrirMenuBtn = document.querySelector(".abrirMenu-responsive");
 const cerrarMenuBtn = document.querySelector(".cerrarMenu-responsive");
 const redesSociales = document.querySelector(".social-bar");
 
 function abrir() {
-
     redesSociales.classList.toggle("menu-abierto");
     // redesSociales.classList.remove("menu-cerrado");
-    
-
-
-
 }
 function cerrar() {
-
     redesSociales.classList.remove("menu-abierto");
     // redesSociales.classList.add("menu-cerrado");
     // redesSociales.style.transition="width 0.5s";
-
 }
-
 abrirMenuBtn.addEventListener("click",abrir);
 cerrarMenuBtn.addEventListener("click",cerrar);
