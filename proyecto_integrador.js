b10623b0e099f27f76f25e08347a2534e722b99b
Rubia <rj-rosales@hotmail.com>
2022-07-10 17:43:34 -0300

    Agregué un inicio de sesion sin backend, reistro y recupero de contraseña

--- proyecto_integrador.js
+++ proyecto_integrador.js
@@ -14,3 +14,48 @@ function cerrar() {
 }
 abrirMenuBtn.addEventListener("click",abrir);
 cerrarMenuBtn.addEventListener("click",cerrar);
+
+
+//SCRIPT DEL INICIO DE SESIÓN
+const entrarBtn = document.querySelector(".login-logo");
+const salirBtn = document.querySelector(".logout-logo");
+const inicioDeSesion = document.querySelector(".login-section");
+
+function entrar() {
+    inicioDeSesion.style.display="flex";
+    entrarBtn.style.display="none";
+    salirBtn.style.display="flex";
+}
+function salir() {
+    inicioDeSesion.style.display="none";
+    entrarBtn.style.display="flex";
+    salirBtn.style.display="none";
+    recuperarContraseña.style.display="none";
+    registrarse.style.display="none";
+    
+}
+entrarBtn.addEventListener("click",entrar);
+salirBtn.addEventListener("click",salir);
+
+//SCRIPT DEL REGISTRO
+const registro = document.querySelector(".registro");
+const registrarse = document.querySelector(".register-section");
+
+function registrar() {
+    registrarse.style.display="flex";
+    inicioDeSesion.style.display="none";
+}
+
+registro.addEventListener("click",registrar);
+
+//SCRIPT DEL RECUPERO
+const recupero = document.querySelector(".recupero");
+const recuperarContraseña = document.querySelector(".forgot-section");
+
+function recuperar() {
+    recuperarContraseña.style.display="flex";
+    inicioDeSesion.style.display="none";
+    registrarse.style.display="none";
+}
+
+recupero.addEventListener("click",recuperar);
