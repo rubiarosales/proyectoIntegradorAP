a61fc3f0f583dc36517a3851971992a3ffb048f3
Rubia <rj-rosales@hotmail.com>
2023-03-18 13:29:42 -0300

    Cambié la URL del ambiente de produccion por la de Render

--- Backend/rrosales/Dockerfile
+++ Backend/rrosales/Dockerfile
@@ -2,3 +2,4 @@ FROM amazoncorretto:17-alpine-jdk
 MAINTAINER rubiarosales
 COPY target/rrosales-0.0.1-SNAPSHOT.jar rrosales-app.jar
 ENTRYPOINT ["java","-jar","/rrosales-app.jar"]
+EXPOSE 8080
--- Frontend/PI_Angular/src/environments/environment.prod.ts
+++ Frontend/PI_Angular/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  URL: 'https://backend-ap-rr.herokuapp.com/'
+  // URL: 'https://backend-ap-rr.herokuapp.com/'
+  URL:'https://proyectointegradorap.onrender.com/'
 };
